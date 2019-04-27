5fecc01d025e829803d1d906ef388333cf6b5da8
Kowah <1095036995@qq.com>
2019-04-28 00:15:11 +0800

    index change

---
+++ src/main/java/com/kowah/habitapp/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.kowah.habitapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+    @RequestMapping("/")
+    public String Index(){
+        return "forward://protocol.html";
+    }
+}
